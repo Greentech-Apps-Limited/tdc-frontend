6f15ec3d8f133dba6c75192ad5ab3b88b4b92568
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-08-07 20:15:05 +0600

    add homepage banner and layout

--- app/(quran)/page.tsx
+++ app/(quran)/page.tsx
@@ -1,7 +1,12 @@
+import Banner from '@/components/banner';
+
 export default function Home() {
   return (
-    <main>
-      <p>Quran</p>
+    <main className="space-y-6">
+      <Banner />
+      <section>
+        <p>Quran</p>
+      </section>
     </main>
   );
 }
---
+++ components/banner.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <section>
+      <Image src="logos/logo.svg" width={248} height={136} alt="Brand logo" />
+    </section>
+  );
+};
+
+export default Banner;
