45c136a740a0ac636365be7b1072592673028152
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-08-19 17:58:17 +0600

    added versed data

--- app/(quran)/[quranSegment]/[segmentId]/page.tsx
+++ app/(quran)/[quranSegment]/[segmentId]/page.tsx
@@ -1,7 +1,21 @@
-const QuranSegmentDetails = () => {
+import { readData } from '@/lib/read-file';
+import { QuranSegment } from '@/lib/types/quran-segment-type';
+
+type QuranSegmentDetails = {
+  params: {
+    quranSegment: QuranSegment;
+    segmentId: string;
+  };
+};
+
+const QuranSegmentDetails = async ({ params }: QuranSegmentDetails) => {
+  const quranMeta = await readData(`data/verses/surah_id_${params.segmentId}.json`);
+  console.log(quranMeta);
   return (
     <div>
-      <p>side</p>
+      {quranMeta.verses.map(item => {
+        return <div key={item.id}>{item.text_uthmani}</div>;
+      })}
     </div>
   );
 };
