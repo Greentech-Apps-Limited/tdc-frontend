1a51070ff751dac7cec02bb492293ee7081c27a7
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-08-26 20:09:20 +0600

    add settings layout

---
+++ components/settings/settings.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '../ui/sheet';
+import { SettingsIcon } from '@/icons';
+
+const Settings = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <SettingsIcon className="text-2xl hover:cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Settings</SheetTitle>
+        </SheetHeader>
+        <div>Test</div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Settings;
