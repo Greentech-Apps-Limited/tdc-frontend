ed252e97deeba5c151eb43586cf3ce302db8ce1e
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-08-31 14:00:03 +0600

    add setting checker

--- contexts/settings-provider.tsx
+++ contexts/settings-provider.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-// import { useCheckSettingsDiff } from '@/hooks/use-check-settings-diff';
-// import { useUpdateSearchParams } from '@/hooks/use-update-search-params';
 import useSettingsStore, { SettingsActions, SettingsState } from '@/stores/settings-store';
 import { createContext, useContext, useRef } from 'react';
 
@@ -9,13 +7,6 @@ const SettingsStoreContext = createContext<typeof useSettingsStore | null>(null)
 
 export function SettingsProvider({ children }: { children: React.ReactNode }) {
   const storeRef = useRef(useSettingsStore);
-  // const updateSearchParams = useUpdateSearchParams();
-  // const checkSettingsDiff = useCheckSettingsDiff();
-
-  // const { wbwDiff, translationsDiff, wbwTr, selectedTranslation } = checkSettingsDiff();
-  // if (wbwDiff || translationsDiff) {
-  //   updateSearchParams(wbwTr || 'en', selectedTranslation || [20]);
-  // }
 
   return (
     <SettingsStoreContext.Provider value={storeRef.current}>
