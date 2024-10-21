6bc467e7b5ab3926d5634a6b4190efe78c74379a
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-10-21 21:34:25 +0600

    add suspence and fallback

--- app/[locale]/(quran)/layout.tsx
+++ app/[locale]/(quran)/layout.tsx
@@ -2,6 +2,8 @@ import AudioPlayerWrapper from '@/components/audio-player/audio-player-wrapper';
 import Header from '@/components/header';
 import { SettingsChecker } from '@/components/settings/settings-checker';
 import Sidebar from '@/components/sidebar';
+import HeaderSkeleton from '@/components/skeleton-loaders/header-skeleton';
+import SidebarSkeleton from '@/components/skeleton-loaders/sidebar-skeleton';
 import { unstable_setRequestLocale } from 'next-intl/server';
 import { Suspense } from 'react';
 
@@ -11,22 +13,27 @@ const HomeLayout = ({
 }: Readonly<{ children: React.ReactNode; params: { locale: string } }>) => {
   unstable_setRequestLocale(locale);
   return (
-    <Suspense>
-      <SettingsChecker>
-        <section className="flex h-full w-full">
+    <>
+      <Suspense>
+        <SettingsChecker />
+      </Suspense>
+      <section className="flex h-full w-full">
+        <Suspense fallback={<SidebarSkeleton />}>
           <Sidebar />
-          <div className="flex h-full w-full flex-col">
+        </Suspense>
+        <div className="flex h-full w-full flex-col">
+          <Suspense fallback={<HeaderSkeleton />}>
             <Header />
-            <div className="relative h-full w-full flex-1 overflow-hidden">
-              {children}
-              <div className="fixed bottom-0 right-0 z-10">
-                <AudioPlayerWrapper />
-              </div>
+          </Suspense>
+          <div className="relative h-full w-full flex-1 overflow-hidden">
+            {children}
+            <div className="fixed bottom-0 right-0 z-10">
+              <AudioPlayerWrapper />
             </div>
           </div>
-        </section>
-      </SettingsChecker>
-    </Suspense>
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ components/skeleton-loaders/home-page-skeleton.tsx
@@ -0,0 +1,35 @@
+import { Skeleton } from '../ui/skeleton';
+
+const BannerSkeleton = () => {
+  return (
+    <section>
+      <Skeleton className="h-[136px] w-[248px]" />
+    </section>
+  );
+};
+
+const SurahRowSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {[...Array(9)].map((_, index) => (
+        <div
+          key={index}
+          className="flex items-center justify-between rounded-full border border-neutral-200 bg-neutral p-2 pr-6"
+        >
+          <div className="flex gap-3">
+            <Skeleton className="h-[52px] w-[52px] rounded-full" />
+            <div>
+              <Skeleton className="mb-2 h-6 w-32" />
+              <Skeleton className="h-4 w-24" />
+            </div>
+          </div>
+          <Skeleton className="h-[30px] w-[80px]" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const QuickAccessSectionSkeleton = () => <Skeleton className="w-ful h-72 rounded-4xl" />;
+
+export { BannerSkeleton, SurahRowSkeleton, QuickAccessSectionSkeleton };
