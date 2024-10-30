a216cc57f6e7592dbfc8ed0c497a41795aa32ac4
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-10-30 19:35:26 +0600

    update play-mode Meta

--- app/[locale]/quiz/play-mode/layout.tsx
+++ app/[locale]/quiz/play-mode/layout.tsx
@@ -1,10 +1,3 @@
-import constructMetadata from '@/lib/metadata';
-
-export const metadata = constructMetadata({
-  title: 'Play Mode',
-  description: 'Play Quranic Quiz',
-});
-
 const QuranicQuizPlayModeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return <section className="h-screen w-screen">{children}</section>;
 };
