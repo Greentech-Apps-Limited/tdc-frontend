b7593c39ed0c20c2c082f61a5e2bbb73f6f8dc8d
Maharab Hossain Opi <maharabhossain0022@gmail.com>
2024-11-08 15:27:52 +0600

    add Image cache

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [80, 96, 128, 256],
     imageSizes: [16, 32, 48, 64],
-    minimumCacheTTL: 60,
+    minimumCacheTTL: 31536000,
   },
   webpack(config) {
     config.module.rules.push({
